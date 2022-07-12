da50a9ed7592d42cd73203f363671bbc908e542d
Aroon Salotra <aroonsalotra@gmail.com>
2022-07-12 03:30:27 +0100

    add more mobile responsiveness

--- src/Info.jsx
+++ src/Info.jsx
@@ -6,20 +6,22 @@ import { useState } from "react";
 
 const Info = (props) => {
     const [display, setDisplay] = useState("none")
+    const [color, setColor] = useState("white")
 
     const handleClick = () => {
         (props.display === "block" ? props.setDisplay("none") : props.setDisplay("block"))
     }
 
     const handleClick2 = () => {
-      (display === "block" ? setDisplay("none") : setDisplay("block") )
+        (display === "block" ? setDisplay("none") : setDisplay("block"))
     }
 
 
 
     return (
         <div className="info">
-            <h1 id="infoHeader">Program Information  <BsFillInfoCircleFill onClick={() => handleClick2()} className="icon-m"  /> </h1>
+            <h1 id="infoHeader" style={{zIndex: "2"}}>Program Information  <BsFillInfoCircleFill onClick={() => handleClick2()}
+                display={{ color: color }} className="icon-m" /> </h1>
             <h2 id="infoSubtitle">5/3/1 - 3 Day Program</h2>
             {/* <p id="infoDesc">Each day, pick a push or pull day and follow the lifts
                 a week is finished once you have ran 3 days for that week, alternating between push and pull days
@@ -28,11 +30,11 @@ const Info = (props) => {
                 {/* <p>To find out your calories, press this icon</p> */}
                 {/* <BiBorderOuter className="icon-m" id="btn-calc" onClick={() => handleClick()} /> */}
                 <p>To find out your calories, press <span onClick={() => handleClick()} className="link">here</span> </p>
-                <div id="infoPopup" style={{display: display}}>
-                <h2>How to run</h2>
-                <p>enter your 1RM (One Rep Max) in the boxes, you will then see your numbers
-                for the program. Every last set is AMAP (As many reps as possible)
-                for assistance, pick any amount of exercise to reach 100 total reps</p>
+                <div id="infoPopup" style={{ display: display }}>
+                    <h2>How to run</h2>
+                    <p>enter your 1RM (One Rep Max) in the boxes, you will then see your numbers
+                        for the program. Every last set is AMAP (As many reps as possible)
+                        for assistance, pick any amount of exercise to reach 100 total reps</p>
                 </div>
             </div>
         </div>
